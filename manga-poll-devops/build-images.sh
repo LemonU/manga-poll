4ecab8b69b030be2633535d2680b82d926516bfb
LemonU <lassaegis@gmail.com>
2021-06-13 16:11:56 -0400

    fixed issue of not able to access broker

--- manga-poll-devops/build-images.sh
+++ manga-poll-devops/build-images.sh
@@ -1,3 +1,3 @@
-docker build -t manga-poll:zookeeper -f Zookeeper.Dockerfile .
-docker build -t manga-poll:broker -f Broker.Dockerfile .
-docker build -t manga-poll:connect -f Connect.Dockerfile .
+docker build -t lemonu/manga-poll:zookeeper -f Zookeeper.Dockerfile .
+docker build -t lemonu/manga-poll:broker -f Broker.Dockerfile .
+docker build -t lemonu/manga-poll:connect -f Connect.Dockerfile .
